ef48543f127ac641b978289cc3a48228a26b9ad8
Aron <aronwc@gmail.com>
2017-09-20 12:07:08 -0500

    bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ test_requirements = [
 
 setup(
     name='twclassify',
-    version='0.1.1',
+    version='0.1.2',
     description=".",
     long_description=readme + '\n\n' + history,
     author="Aron Culotta",
